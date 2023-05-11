366a75a7fd57dbcdb811fc10c29dfe5db892e29b
RachanaKS <97619203+RachanaKS@users.noreply.github.com>
2023-05-11 11:49:27 +0530

    Update samle.sh

--- samle.sh
+++ samle.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+# Run the first YAML file
+echo "Running first YAML file..."
+https://github.com/RachanaKS/sample/blob/main/.github/workflows/releasesample1.yml
+
+# Run the second YAML file
+echo "Running second YAML file..."
+https://github.com/RachanaKS/sample/blob/main/.github/workflows/releasesample2.yml
+
+# Run the third YAML file
+echo "Running third YAML file..."
+https://github.com/RachanaKS/sample/blob/main/.github/workflows/releasesample3.yml
